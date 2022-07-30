8ea8d520b2fbf869291e27d3d79d0d5a2e17b99d
YawOtuo <yotuo2003@gmail.com>
2022-07-30 20:23:17 +0000

    base designs

---
+++ firsttune/urls.py
@@ -0,0 +1,21 @@
+from . import views
+from django.urls import path
+from django.views.generic.edit import CreateView
+
+
+app_name = 'firsttune'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    # path('featured/', views.FeaturedView.as_view(), name='featured'),
+    # path('team/', views.TeamView.as_view(), name='team'),
+    # path('contact/', views.ContactView.as_view(), name='contact'),
+
+
+
+    
+
+    
+]
+
+
---
+++ firsttune/views.py
@@ -0,0 +1,41 @@
+
+# Create your views here.
+from django.shortcuts import render
+from typing import FrozenSet
+from django import template
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db.models.query import QuerySet
+from django.forms.models import inlineformset_factory
+from django.shortcuts import get_object_or_404, redirect, render, redirect, resolve_url
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect, request
+from django.urls import reverse
+from django.views import generic
+from django.views.generic.edit import CreateView, FormView, UpdateView
+from django.views.generic.list import ListView
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.conf import settings 
+# from bootstrap_datepicker_plus import DatePickerInput
+from django.core.mail import send_mail
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+
+class IndexView(TemplateView):
+    template_name = "firsttune/index.html"
+    
+
+# class FeaturedView(TemplateView):
+#     template_name = 'michael/featured.html'
+
+
+# class TeamView(TemplateView):
+#     template_name = 'michael/team.html'
+
+# class ContactView(TemplateView):
+#     template_name = 'michael/contact.html'
