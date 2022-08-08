9755c33a9dc34dda4c9fff386e46e8cb7a9b8ce0
YawOtuo <yotuo2003@gmail.com>
2022-08-08 21:03:20 +0000

    adding hyperlinks and responsive blog post

--- static/firsttune/index.js
+++ static/firsttune/index.js
@@ -1,7 +1,9 @@
 window.onload = (event) => {
-    // fadeIn()
+    fadeIn()
 
-    // setTimeout(remove, 2500)
+    setTimeout(slideOut, 2500)
+    // setTimeout(slideOut, 2500)
+
     // hide_wait_small()
     // setTimeout(show_wait_small, 1500)
 
@@ -14,14 +16,24 @@ fadeIn =() => {
     homerow.classList.add("animate__animated")
     homerow.classList.add("animate__slow")
 
-    homerow.classList.add("animate__fadeOut")
+    homerow.classList.add("animate__fadeIn")
 
+}
+
+slideOut = () => {
+    homerow = document.getElementById('homepic')
+    console.log(homerow)
+    homerow.classList.add("animate__animated")
+    homerow.classList.add("animate__slow")
+
+    // homerow.classList.add("animate__slideOutUp")  
+
+    // homerow.classList.add('d-none')
 }
 
 remove = () => {
     homerow = document.getElementById('homepic')
-
-    homerow.classList.add("d-none")
+    homerow.classList.add('d-none')
 
 }
 
